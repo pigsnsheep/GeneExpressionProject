211d818caeb2136013f9a77279523edbdc5ffbe4
pigsnsheep <47608502+pigsnsheep@users.noreply.github.com>
2022-03-23 10:56:35 -0700

    started adding dupe removing function

--- Setup.R
+++ Setup.R
@@ -2,9 +2,30 @@
 library(tidyverse)
 library(ggplot2)
 genedata_csv <- read.csv("/Users/pigsnsheep/Downloads/Gene\ Expression\ Project/CCLE_expression.csv", header = TRUE)
+genedata_csv2 <- read.csv("/Users/pigsnsheep/Downloads/Gene\ Expression\ Project/CCLE_expression.csv")
 genedata_df <- t(data.frame(genedata_csv))
+genedata_df2 <- t(data.frame(genedata_csv2))
 genedata_matrix <- data.matrix(genedata_df)
 genedata_means <- read.csv("Means.csv")
 genedata_stdevs <- read.csv("Standard Deviations.csv")
-genedata_genes <- genedata_df[1,]
+genedata_genes <- genedata_df2[c(2:19178),1]
+dupes <- c()
+
+for (gene in genedata_genes){
+  if (is.null(dupes[gene])){
+    dupes[gene] <- 1
+  }
+  else {
+    dupes[gene] <- dupes[gene] + 1
+  }
+}
+
+dupes
+
+if(is.null(dupes[genedata_genes[2]])){
+  dupes[genedata_genes[2]] <- 1
+}
+dupes
+
+genedata_genes[2]
 
