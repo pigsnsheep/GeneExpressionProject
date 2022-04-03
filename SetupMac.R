cbf1fc29aa8f173b90a274b9c9f78f783dafd772
pigsnsheep <47608502+pigsnsheep@users.noreply.github.com>
2022-04-03 01:04:50 -0700

    Cleanup + Kept proper genes

--- SetupMac.R
+++ SetupMac.R
@@ -3,30 +3,11 @@ library(tidyverse)
 library(ggplot2)
 genedata_csv <- read.csv("/Users/pigsnsheep/Downloads/Gene\ Expression\ Project/CCLE_expression.csv", header = TRUE)
 genedata_csv2 <- read.csv("/Users/pigsnsheep/Downloads/Gene\ Expression\ Project/CCLE_expression.csv")
-genedata_df <- t(data.frame(genedata_csv))
-genedata_df2 <- t(data.frame(genedata_csv2))
-genedata_matrix <- data.matrix(genedata_df)
-genedata_means <- read.csv("Means.csv")
-genedata_stdevs <- read.csv("Standard Deviations.csv")
-genedata_genes <- rownames(genedata_df)
-dupes <- c()
-dupes[1] <- genedata_genes[1]
-dupes[1]
-for (gene in genedata_genes){
-  if (is.null(dupes[gene])){
-    dupes[gene] <- 1
-  }
-  else {
-    dupes[gene] <- dupes[gene] + 1
-  }
-}
-
-dupes
-
-if(is.null(dupes[genedata_genes[2]])){
-  dupes[genedata_genes[2]] <- 1
-}
-dupes
-
-genedata_genes[2]
+genedata_means <- read.csv("Means.csv")[2]
+genedata_stdevs <- read.csv("Standard Deviations.csv")[2]
+genedata_genes <- read.csv("Genes.csv")[2]
+kept_genes <- read.csv("Kept Genes.csv")
+kept_means <- read.csv("Kept Means.csv")
+kept_standard_deviations <- read.csv("Kept Standard Deviations.csv")
+gene_scores <- genedata_means + genedata_stdevs
 
--- Visualizations.R
+++ Visualizations.R
@@ -1,2 +1,2 @@
-ggplot(data=NULL, aes(x=genedata_stdevs)) + geom_histogram(bins = 200)
-ggplot(data=NULL, aes(x=genedata_means)) + geom_histogram(bins = 200)
+ggplot(data=kept_standard_deviations, aes(x=x)) + geom_histogram(bins = 200) + geom_vline(xintercept = 0.7)
+ggplot(data=genedata_means, aes(x=x)) + geom_histogram(bins = 200) + geom_vline(xintercept = 0.5)
