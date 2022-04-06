a0996aff5c5cea7d6361d28855f78d21ac31184d
pigsnsheep <47608502+pigsnsheep@users.noreply.github.com>
2022-04-05 18:30:52 -0700

    Cleanup and added some visualizations

--- SetupMac.R
+++ SetupMac.R
@@ -6,21 +6,19 @@ genedata_csv2 <- read.csv("/Users/pigsnsheep/Downloads/Gene\ Expression\ Project
 drugdata_csv <- read.csv("/Users/pigsnsheep/Downloads/Gene\ Expression\ Project/primary-screen-replicate-collapsed-logfold-change.csv")
 drugdata_csv2 <- t(data.frame(drugdata_csv)[-c(4,17,189,221,257,270,423,500,545,559),])
 drugdata_matrix <- data.frame(matrix(as.numeric(matrix(drugdata_csv2, 4687, 568)[-c(1), ]), 4686, 568))
-drugdata_drugs <- rownames(drugdata_csv2)
-drugdata_means <- rowMeans(drugdata_matrix, na.rm = TRUE)
+drugdata_drugs <- read.csv("Drugs.csv")[2]
+drugdata_means <- read.csv("DrugMeans.csv")[2]
 genedata_means <- read.csv("Means.csv")[2]
 genedata_stdevs <- read.csv("Standard Deviations.csv")[2]
 genedata_genes <- read.csv("Genes.csv")[2]
 kept_genes <- read.csv("Kept Genes.csv")[2]
 kept_means <- read.csv("Kept Means.csv")[2]
+kept_drugs <- read.csv("Kept Drugs.csv")[2]
+drugdata_sds <- read.csv("DrugSds.csv")[2]
 kept_standard_deviations <- read.csv("Kept Standard Deviations.csv")[2]
 gene_scores <- genedata_means + genedata_stdevs
 
-sum(duplicated(drugdata_drugs) == TRUE)
-mean(drugdata_csv2[2,], na.rm = TRUE)
-drugdata_csv2[,1]
 
-write.csv(drugdata_drugs, "Drugs.csv")
-drugdata_sds <- apply(drugdata_matrix, 1, sd, na.rm = TRUE)
-write.csv(drugdata_means, "DrugMeans.csv")
-write.csv(drugdata_sds, "DrugSds.csv")
+
+
+
--- Visualizations.R
+++ Visualizations.R
@@ -1,2 +1,3 @@
-ggplot(data=kept_standard_deviations, aes(x=x)) + geom_histogram(bins = 200) + geom_vline(xintercept = 0.7)
-ggplot(data=genedata_means, aes(x=x)) + geom_histogram(bins = 200) + geom_vline(xintercept = 0.5)
+ggplot(data=drugdata_sds, aes(x=x)) + geom_histogram(bins = 200) + geom_vline(xintercept = 0.5)
+ggplot(data=drugdata_means, aes(x=x)) + geom_histogram(bins = 200) + geom_vline(xintercept = 0.77)
+
